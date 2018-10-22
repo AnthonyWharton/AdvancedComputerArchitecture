ea63c84847afce193519f17b4f73ba3a66b2348c
Anthony Wharton <a.wharton.2015@bristol.ac.uk>
2018-10-22 20:41:05 +0100

    Remove static lifetype restriction

--- src/util/exit.rs
+++ src/util/exit.rs
@@ -2,17 +2,19 @@ use std::process;
 
 pub enum Exit {
     ArgumentError,
+    FileLoadError,
 }
 
 impl Exit {
-    pub fn exit(self, message: Option<&'static str>) -> ! {
+    pub fn exit(self, message: Option<&str>) -> ! {
         match self {
             Exit::ArgumentError => exit(1, message.unwrap_or("Argument Error!")),
+            Exit::FileLoadError => exit(1, message.unwrap_or("File Load Error!")),
         }
     }
 }
 
-fn exit(code: i32, message: &'static str) -> ! {
+fn exit(code: i32, message: &str) -> ! {
     if message.len() != 0 {
         if code != 0 {
             println!("Error: {}", message);
