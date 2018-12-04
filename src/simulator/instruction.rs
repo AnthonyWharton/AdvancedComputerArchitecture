6ce975e87a2a0cdbc3d502f8fbf251efa16eb930
Anthony Wharton <a.wharton.2015@bristol.ac.uk>
2018-12-04 14:31:38 +0000

    Add execute unit enumeration

--- src/simulator/instruction.rs
+++ src/simulator/instruction.rs
@@ -4,6 +4,90 @@ use isa::operand::Register;
 use super::state::State;
 
 ///////////////////////////////////////////////////////////////////////////////
+//// ENUMS
+
+/// An enumeration of the different types of execute units that exist within
+/// the simulator.
+pub enum ExecuteUnit {
+    /// **Arithmentic Logic Unit**, Responsible for all arithmetic and logic
+    /// operations.
+    ALU,
+    /// **Branch Logic Unit**, Responsible for any operations that will touch
+    /// the program counter, causing the program to jump or branch to other
+    /// instructions.
+    BLU,
+    /// **Memory & Control Unit**, Responsible for load and store operations
+    /// that happen with main memory, as well as control operations and system
+    /// calls.
+    MCU,
+}
+
+///////////////////////////////////////////////////////////////////////////////
+//// IMPLEMENTATIONS
+
+impl From<Operation> for ExecuteUnit {
+    fn from(op: Operation) -> ExecuteUnit {
+        match op {
+            Operation::LUI    => ExecuteUnit::ALU,
+            Operation::AUIPC  => ExecuteUnit::BLU,
+            Operation::JAL    => ExecuteUnit::BLU,
+            Operation::JALR   => ExecuteUnit::BLU,
+            Operation::BEQ    => ExecuteUnit::BLU,
+            Operation::BNE    => ExecuteUnit::BLU,
+            Operation::BLT    => ExecuteUnit::BLU,
+            Operation::BGE    => ExecuteUnit::BLU,
+            Operation::BLTU   => ExecuteUnit::BLU,
+            Operation::BGEU   => ExecuteUnit::BLU,
+            Operation::LB     => ExecuteUnit::MCU,
+            Operation::LH     => ExecuteUnit::MCU,
+            Operation::LW     => ExecuteUnit::MCU,
+            Operation::LBU    => ExecuteUnit::MCU,
+            Operation::LHU    => ExecuteUnit::MCU,
+            Operation::SB     => ExecuteUnit::MCU,
+            Operation::SH     => ExecuteUnit::MCU,
+            Operation::SW     => ExecuteUnit::MCU,
+            Operation::ADDI   => ExecuteUnit::ALU,
+            Operation::SLTI   => ExecuteUnit::ALU,
+            Operation::SLTIU  => ExecuteUnit::ALU,
+            Operation::XORI   => ExecuteUnit::ALU,
+            Operation::ORI    => ExecuteUnit::ALU,
+            Operation::ANDI   => ExecuteUnit::ALU,
+            Operation::SLLI   => ExecuteUnit::ALU,
+            Operation::SRLI   => ExecuteUnit::ALU,
+            Operation::SRAI   => ExecuteUnit::ALU,
+            Operation::ADD    => ExecuteUnit::ALU,
+            Operation::SUB    => ExecuteUnit::ALU,
+            Operation::SLL    => ExecuteUnit::ALU,
+            Operation::SLT    => ExecuteUnit::ALU,
+            Operation::SLTU   => ExecuteUnit::ALU,
+            Operation::XOR    => ExecuteUnit::ALU,
+            Operation::SRL    => ExecuteUnit::ALU,
+            Operation::SRA    => ExecuteUnit::ALU,
+            Operation::OR     => ExecuteUnit::ALU,
+            Operation::AND    => ExecuteUnit::ALU,
+            Operation::FENCE  => ExecuteUnit::MCU,
+            Operation::FENCEI => ExecuteUnit::MCU,
+            Operation::ECALL  => ExecuteUnit::MCU,
+            Operation::EBREAK => ExecuteUnit::MCU,
+            Operation::CSRRW  => ExecuteUnit::MCU,
+            Operation::CSRRS  => ExecuteUnit::MCU,
+            Operation::CSRRC  => ExecuteUnit::MCU,
+            Operation::CSRRWI => ExecuteUnit::MCU,
+            Operation::CSRRSI => ExecuteUnit::MCU,
+            Operation::CSRRCI => ExecuteUnit::MCU,
+            Operation::MUL    => ExecuteUnit::ALU,
+            Operation::MULH   => ExecuteUnit::ALU,
+            Operation::MULHSU => ExecuteUnit::ALU,
+            Operation::MULHU  => ExecuteUnit::ALU,
+            Operation::DIV    => ExecuteUnit::ALU,
+            Operation::DIVU   => ExecuteUnit::ALU,
+            Operation::REM    => ExecuteUnit::ALU,
+            Operation::REMU   => ExecuteUnit::ALU,
+        }
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////
 //// FUNCTIONS
 
 /// Main entry point for execution given an instruction, state and memory.
@@ -157,20 +241,30 @@ fn exec_i_type(state: &mut State, instruction: Instruction) {
     }
 
     r[rd] = match instruction.op {
-        Operation::LB    => m[(r[rs1] + imm) as usize] as i8 as i32,
-        Operation::LH    => m.read_i16((r[rs1] + imm) as usize).word as i32,
-        Operation::LW    => m.read_i32((r[rs1] + imm) as usize).word,
-        Operation::LBU   => m[(r[rs1] + imm) as usize] as i32,
-        Operation::LHU   => m.read_u16((r[rs1] + imm) as usize).word as i32,
-        Operation::ADDI  => r[rs1] + imm,
-        Operation::SLTI  => (r[rs1] < imm) as i32,
-        Operation::SLTIU => ((r[rs1] as u32) < (imm as u32)) as i32,
-        Operation::XORI  => r[rs1] ^ imm,
-        Operation::ORI   => r[rs1] | imm,
-        Operation::ANDI  => r[rs1] & imm,
-        Operation::SLLI  => r[rs1] << imm,
-        Operation::SRLI  => ((r[rs1] as u32) >> (imm as u32)) as i32,
-        Operation::SRAI  => r[rs1] >> (imm & 0b11111),
+        Operation::LB     => m[(r[rs1] + imm) as usize] as i8 as i32,
+        Operation::LH     => m.read_i16((r[rs1] + imm) as usize).word as i32,
+        Operation::LW     => m.read_i32((r[rs1] + imm) as usize).word,
+        Operation::LBU    => m[(r[rs1] + imm) as usize] as i32,
+        Operation::LHU    => m.read_u16((r[rs1] + imm) as usize).word as i32,
+        Operation::ADDI   => r[rs1] + imm,
+        Operation::SLTI   => (r[rs1] < imm) as i32,
+        Operation::SLTIU  => ((r[rs1] as u32) < (imm as u32)) as i32,
+        Operation::XORI   => r[rs1] ^ imm,
+        Operation::ORI    => r[rs1] | imm,
+        Operation::ANDI   => r[rs1] & imm,
+        Operation::SLLI   => r[rs1] << imm,
+        Operation::SRLI   => ((r[rs1] as u32) >> (imm as u32)) as i32,
+        Operation::SRAI   => r[rs1] >> (imm & 0b11111),
+        Operation::FENCE  => unimplemented!(),
+        Operation::FENCEI => unimplemented!(),
+        Operation::ECALL  => unimplemented!(),
+        Operation::EBREAK => unimplemented!(),
+        Operation::CSRRW  => unimplemented!(),
+        Operation::CSRRS  => unimplemented!(),
+        Operation::CSRRC  => unimplemented!(),
+        Operation::CSRRWI => unimplemented!(),
+        Operation::CSRRSI => unimplemented!(),
+        Operation::CSRRCI => unimplemented!(),
         _ => panic!("Unknown I type instruction failed to execute.")
     };
 
