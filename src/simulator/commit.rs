0acf0b09c74b17b55c16185bb669cd20270abe88
Anthony Wharton <a.wharton.2015@bristol.ac.uk>
2018-12-15 12:00:58 +0000

    Add commit I-type instructions

--- src/simulator/commit.rs
+++ src/simulator/commit.rs
@@ -1,6 +1,7 @@
+use either::{Left, Right};
+
 use crate::isa::op_code::Operation;
-use crate::isa::operand::Register;
-use crate::isa::{Format, Instruction};
+use crate::isa::Format;
 
 use super::reorder::ReorderEntry;
 use super::state::State;
@@ -35,70 +36,46 @@ fn cm_r_type(state: &mut State, rob_entry: &ReorderEntry) {
         state.register.finished_write(rob_entry.reg_rd.unwrap(), rob_entry.name_rd.unwrap());
     } else {
         // Branch prediction failure
+        panic!("Did not expect R type instruction to have mismatching PC!")
     }
 }
 
 /// Commits an I type instruction from a reorder buffer entry to the given
 /// state.
 fn cm_i_type(state: &mut State, rob_entry: &ReorderEntry) {
-    // let rs1_s = match r.rs1 {
-    //     Left(val) => val,
-    //     Right(name) => rf.read_at_name(name).expect("Execute unit missing rs1!"),
-    // };
-    // let rs1_u = rs1_s as u32;
-    // let imm_s = r.imm.expect("Execute unit missing imm!");
-    // let imm_u = imm_s as u32;
+    let rs1_s = match rob_entry.rs1 {
+        Left(val) => val,
+        Right(name) => {
+            let r = state.register.read_at_name(name).expect("Commit unit missing rs1!");
+            state.register.finished_read(name);
+            r
+        }
+    };
+    let imm_s = rob_entry.imm.expect("Commit unit missing imm!");
 
-    // #[rustfmt::skip]
-    // let rd_val = match r.op {
-    //     Operation::JALR   => Some(rf.read_reg(Register::PC).unwrap() + 4),
-    //     // TODO Move to writeback stage
-    //     // Operation::LB     => m[(rs1 + imm) as usize] as i8 as i32,
-    //     // Operation::LH     => m.read_i16((rs1 + imm) as usize).word as i32,
-    //     // Operation::LW     => m.read_i32((rs1 + imm) as usize).word,
-    //     // Operation::LBU    => m[(rs1 + imm) as usize] as i32,
-    //     // Operation::LHU    => m.read_u16((rs1 + imm) as usize).word as i32,
-    //     Operation::LB     => None,
-    //     Operation::LH     => None,
-    //     Operation::LW     => None,
-    //     Operation::LBU    => None,
-    //     Operation::LHU    => None,
-    //     Operation::ADDI   => Some( rs1_s +  imm_s),
-    //     Operation::SLTI   => Some((rs1_s <  imm_s) as i32),
-    //     Operation::SLTIU  => Some((rs1_u <  imm_u) as i32),
-    //     Operation::XORI   => Some( rs1_s ^  imm_s),
-    //     Operation::ORI    => Some( rs1_s |  imm_s),
-    //     Operation::ANDI   => Some( rs1_s &  imm_s),
-    //     Operation::SLLI   => Some( rs1_s << imm_s),
-    //     Operation::SRLI   => Some((rs1_u >> imm_u) as i32),
-    //     Operation::SRAI   => Some( rs1_s >> (imm_s & 0b11111)),
-    //     Operation::FENCE  => unimplemented!(),
-    //     Operation::FENCEI => unimplemented!(),
-    //     Operation::ECALL  => unimplemented!(),
-    //     Operation::EBREAK => unimplemented!(),
-    //     Operation::CSRRW  => unimplemented!(),
-    //     Operation::CSRRS  => unimplemented!(),
-    //     Operation::CSRRC  => unimplemented!(),
-    //     Operation::CSRRWI => unimplemented!(),
-    //     Operation::CSRRSI => unimplemented!(),
-    //     Operation::CSRRCI => unimplemented!(),
-    //     _ => panic!("Unknown I type instruction failed to execute.")
-    // };
+    if rob_entry.pc == rob_entry.act_pc {
+        #[rustfmt::skip]
+        let rd_val = match rob_entry.op {
+            Operation::LB  => state.memory[(rs1_s + imm_s) as usize] as i8 as i32,
+            Operation::LH  => state.memory.read_i16((rs1_s + imm_s) as usize).word as i32,
+            Operation::LW  => state.memory.read_i32((rs1_s + imm_s) as usize).word,
+            Operation::LBU => state.memory[(rs1_s + imm_s) as usize] as i32,
+            Operation::LHU => state.memory.read_u16((rs1_s + imm_s) as usize).word as i32,
+            _ => rob_entry.act_rd
+        };
 
-    // let pc_val = if r.op == Operation::JALR {
-    //     (rs1_s + imm_s) & !0b1
-    // } else {
-    //     rf.read_reg(Register::PC).unwrap() + 4
-    // };
+        // Write back to register file
+        state.register.write_to_name(rob_entry.name_rd.unwrap(), rd_val);
+        state.register.finished_write(rob_entry.reg_rd.unwrap(), rob_entry.name_rd.unwrap());
+        // rs1 finished_read() above
+        if let Right(name) = rob_entry.rs2 {
+            state.register.finished_read(name);
+        }
+    } else {
+        // Branch prediction failure
 
-    // self.executing.push_back((
-    //     ExecuteResult {
-    //         rob_entry: r.rob_entry,
-    //         pc: pc_val,
-    //         rd: rd_val,
-    //     },
-    //     ExecutionLen::from(r.op),
-    // ))
+        // TODO
+    }
 }
 
 /// Commits an S type instruction from a reorder buffer entry to the given
