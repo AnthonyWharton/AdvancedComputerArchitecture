b53b4766e0255dd5bc801af0e9152af93673d629
Anthony Wharton <a.wharton.2015@bristol.ac.uk>
2018-12-15 14:51:23 +0000

    Update decode_stage documentation

--- src/simulator/decode.rs
+++ src/simulator/decode.rs
@@ -10,10 +10,13 @@ use super::state::State;
 ///////////////////////////////////////////////////////////////////////////////
 //// FUNCTIONS
 
-/// The decode and rename stage of the pipeline. This will decode
-/// instruction(s) from the previous stage in the pipeline; check for
-/// inter-instruction dependencies; sanitise any dependencies; and then place
-/// into the next stage in the pipeline.
+/// The _Decode & Rename_ stage of the pipeline. This will decode
+/// instruction(s) from the previous
+/// [`LatchFetch`](../fetch/struct.LatchFetch.html) in the pipeline; perform
+/// register renaming; allocate a slot in the
+/// [`ReorderBuffer`](../reorder/struct.ReorderBuffer.html) and then place into
+/// the next stage in the pipeline, the
+/// [`ResvStation`](../reservation/struct.ResvStation.html).
 ///
 /// If sanitisation is not possible, this will stall the pipeline.
 pub fn decode_and_rename_stage(state_p: &State, state: &mut State) {
