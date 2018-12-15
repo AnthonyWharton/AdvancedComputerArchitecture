02604f70a0ff0bced772a781a0bf8e98f4361529
Anthony Wharton <a.wharton.2015@bristol.ac.uk>
2018-12-15 17:12:13 +0000

    Add variants to format_option macro

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -18,10 +18,38 @@ pub mod panic;
 
 /// Formats the contents of an Option if possible, and prints with the given
 /// format specifier. Otherwise formats as "None".
+///
+/// Variants:
+///  - `format_option!(optional)`
+///  - `format_option!(fmt_str, optional)`
+///  - `format_option!(lhs, fmt_str, optional)`
+///  - `format_option!(lhs, fmt_str, rhs, optional)`
+///
+/// Where:
+///  - `optional`: The optional to be formatted.
+///  - `fmt_str`: The format string for the contents of the optional.
+///  - `lhs`/`rhs`: The strings to the left/right hand side of the optional.
 macro_rules! format_option {
+    ($option:expr) => {
+        format!("{:?}", $option)
+    };
+    ($fmt_str:expr, $option:expr) => {
+        if $option.is_some() {
+            format!($fmt_str, $option.clone().unwrap())
+        } else {
+            format!("{:?}", $option)
+        }
+    };
+    ($fmt_lhs:expr, $fmt_str:expr, $option:expr) => {
+        if $option.is_some() {
+            format!(concat!($fmt_lhs, $fmt_str), $option.clone().unwrap())
+        } else {
+            format!(concat!($fmt_lhs, "{:?}"), $option)
+        }
+    };
     ($fmt_lhs:expr, $fmt_str:expr, $fmt_rhs:expr, $option:expr) => {
         if $option.is_some() {
-            format!(concat!($fmt_lhs, $fmt_str, $fmt_rhs), $option.unwrap())
+            format!(concat!($fmt_lhs, $fmt_str, $fmt_rhs), $option.clone().unwrap())
         } else {
             format!(concat!($fmt_lhs, "{:?}", $fmt_rhs), $option)
         }
