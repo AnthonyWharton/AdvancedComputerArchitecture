b980b9938dc11afbdb42d4a722ccf1fdef51d5b8
Anthony Wharton <a.wharton.2015@bristol.ac.uk>
2018-12-18 15:54:49 +0000

    Remove finish checking on fetch stage

--- src/simulator/fetch.rs
+++ src/simulator/fetch.rs
@@ -26,12 +26,8 @@ pub struct LatchFetch {
 /// [`Memory.read_i32()`](../memory/struct.Memory.html#method.write_i32).
 /// Nothing else in the state will be changed.
 pub fn fetch_stage(state_p: &State, state: &mut State) {
-    if state_p.finish_rob_entry.is_none() {
-        let pc = state_p.branch_predictor.get_prediction();
-        let data = Some(state_p.memory.read_i32(pc));
-        state.branch_predictor.predict(data.unwrap().word);
-        state.latch_fetch = LatchFetch { data, pc };
-    } else {
-        state.latch_fetch.data = None;
-    }
+    let pc = state_p.branch_predictor.get_prediction();
+    let data = Some(state_p.memory.read_i32(pc));
+    state.branch_predictor.predict(data.unwrap().word);
+    state.latch_fetch = LatchFetch { data, pc };
 }
