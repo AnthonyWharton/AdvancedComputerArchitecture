39832e82fbfae2fc34471f9638d36b30b66d0bde
Anthony Wharton <a.wharton.2015@bristol.ac.uk>
2018-12-20 01:53:49 +0000

    Update non_recursive fib + Makefiles

--- resources/programs/fib_non_recursive/main.c
+++ resources/programs/fib_non_recursive/main.c
@@ -1,10 +1,16 @@
-void _start()
+unsigned int fib(unsigned int n)
 {
-	volatile int a = 0;
-	int b = 1;
-	for (int i = 3; i <= 8; i++) {
-		int c = b;
+	unsigned int a = 0;
+	unsigned int b = 1;
+	for (int i = 2; i <= n; i++) {
+		unsigned int c = b;
 		b = a + b;
 		a = c;
 	}
+	return b;
+}
+
+void _start()
+{
+	volatile unsigned int foo = fib(9);
 }
